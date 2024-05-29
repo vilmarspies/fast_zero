ab1f259930f788da6c8c74939c7e2e165ea88afa
Vilmar Spies <vilmarspies@gmail.com>
2024-05-29 17:07:58 -0300

    Refatorando estrutura do projeto: Criado routers para Users e Auth; movido constantes para variáveis de ambiente.

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -12,12 +12,12 @@ from zoneinfo import ZoneInfo
 from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.schemas import TokenData
+from fast_zero.settings import Settings
 
-SECRET_KEY = 'your-secret-key'  # Isso é provisório, vamos ajustar!
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+settings = Settings()
+
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 async def get_current_user(
@@ -31,8 +31,10 @@ async def get_current_user(
     )
 
     try:
-        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('sub')
+        payload = decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        print(payload.get('sub'))
         if not username:
             raise credentials_exception
         token_data = TokenData(username=username)
@@ -52,10 +54,12 @@ async def get_current_user(
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
     to_encode.update({'exp': expire})
-    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
     return encoded_jwt
 
 
--- tests/test_security.py
+++ tests/test_security.py
@@ -1,13 +1,27 @@
+import pytest
+from fastapi import HTTPException
 from jwt import decode
 
-from fast_zero.security import SECRET_KEY, create_access_token
+from fast_zero.security import create_access_token, get_current_user, settings
 
 
 def test_jwt():
     data = {'test': 'test'}
     token = create_access_token(data)
 
-    decoded = decode(token, SECRET_KEY, algorithms=['HS256'])
+    decoded = decode(
+        token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+    )
 
     assert decoded['test'] == data['test']
     assert decoded['exp']  # Testa se o valor de exp foi adicionado ao token
+
+
+async def test_get_current_user(session):
+
+    with pytest.raises(HTTPException) as excinfo:
+        await get_current_user(
+            session,
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0ZUB0ZXN0LmNvbSIsImV4cCI6MTcxNzAxMTk2Mn0.niBs8UoCtI-ihQhuUKC45ooTOk86j9mY1H_L0Q9kaaa',
+        )
+    assert str(excinfo.value) == '401: Could not validate credentials'
